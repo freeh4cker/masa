50364a75e36d7edda86976f087c9bf80ba370ee5
freeh4cker <freeh4cker@gmail.com>
2021-12-23 18:13:03 +0100

    :art: refactor certif_valid
    instead to use a delta time
    provide a date at wich the caci should be valable
    it should be more versatile
    foir instance check if CACi is valid at the end of a stage

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
+
 def to_emails(data):
     emails = data.email.to_list()
     return emails
+
 
 def autonomie_absolue_piscine(data):
     data = valid(data)
@@ -36,13 +40,35 @@ def all(data):
 
 
 def _certif_valid(data, delta):
+    """
+
+    :param data:
+    :param int delta:
+    :return:
+    """
     data = data.loc[(data.CACI) | (data.QS)]
     today = pd.to_datetime('today').normalize()
     data = data.loc[today - data.date < pd.Timedelta(f"{delta} days")]
     return data
 
-def valid(data, date=today):
-    return _certif_valid(data, 365)
+
+def _certif_valid_at(data, target_date='today'):
+    """
+
+    :param data:
+    :param int delta:
+    :return:
+    """
+    data = data.loc[(data.CACI) | (data.QS)]
+    target_date = pd.to_datetime(target_date).normalize()
+    data = data.loc[target_date - data.date < 365]
+    return data
+
+
+def valid(data, date='today'):
+    return _certif_valid_at(data)
+
 
 def warning(data):
-    return _certif_valid(data, 350)
+    date= today + 2 weeks
+    return _certif_valid_at(data, target_data=date)
