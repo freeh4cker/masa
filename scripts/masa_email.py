50587e7e94db79992c0d72f48dfa55a1ae735769
freeh4cker <freeh4cker@gmail.com>
2021-12-23 18:15:15 +0100

    :art: refactor main function
    move  arguments parsing from main

--- scripts/masa_email.py
+++ scripts/masa_email.py
@@ -1,36 +1,40 @@
+import sys
 import argparse
 import pandas as pd
 
+import masa
 
 
 def parse_args(args):
-
-
-def main():
-
     parser = argparse.ArgumentParser(description='generate email list')
     parser.add_argument('--autonomie-absolue-piscine')
     parser.add_argument('--autonomie-relative-piscine')
     parser.add_argument('--fosse')
     parser.add_argument('--rifaa')
     parser.add_argument('--no-rifaa')
-    parser.add_argument('--valid'
+    parser.add_argument('--valid',
                         help="list of email with valid caci")
 
     parsed_args = parser.parse_args()
+    return parsed_args
 
-    data = pd.read_csv('path', sep='\t', comment='#')
+
+def main(args=None):
+    args = sys.argv[1:] if args is None else args
+    parsed_args = parse_args(args)
+
+    data = pd.read_csv('./data/masa.tsv', sep='\t', comment='#')
 
     if parsed_args.autonomie_absolute_piscine:
-        emails = autonomie_absolue_piscine(data)
+        emails = masa.autonomie_absolue_piscine(data)
     elif parsed_args.autonomie_relative_piscine:
-        emails = autonomie_relative_piscine(data)
+        emails = masa.autonomie_relative_piscine(data)
     elif parsed_args.fosse:
-        emails = fosse(data)
+        emails = masa.fosse(data)
     elif parsed_args.rifaa:
-        emails = rifaa(data)
+        emails = masa.rifaa(data)
     elif parsed_args.no_rifaa:
-        emails = no_rifaa(data)
+        emails = masa.no_rifaa(data)
     else:
         emails = all(data)
 
