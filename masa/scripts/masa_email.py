9dbf0869d8364e636660b41e5226fc24f10082bc
freeh4cker <freeh4cker@gmail.com>
2021-12-27 11:58:48 +0100

    add header

--- masa/scripts/masa_email.py
+++ masa/scripts/masa_email.py
@@ -1,5 +1,5 @@
 ##########################################################################
-# allow to filter MASA's freedivers given some criteria                  #
+# allow to filter MASA's freedivers given some criteria                 #
 # Copyright (C) 2022  Bertrand Néron                                     #
 #                                                                        #
 # This file is part of masa package.                                     #
