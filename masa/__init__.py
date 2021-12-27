188c984705d3b58c2748ebdfdf2ca3e697eb4824
freeh4cker <freeh4cker@gmail.com>
2021-12-27 11:45:20 +0100

    :art: create masa package

--- masa/__init__.py
+++ masa/__init__.py
@@ -2,40 +2,60 @@ import pandas as pd
 
 
 def to_emails(data):
+    """
+    :param data:
+    :return: the list of emails of freedivesr in data
+    :rtype: list of string
+    """
     emails = data.email.to_list()
     return emails
 
 
-def autonomie_absolue_piscine(data):
-    data = valid(data)
+def autonomie_absolue_piscine(data, date='today'):
+    data = certif_valid_at(data, target_date=date)
     sel = data.loc[
         ((data.indoor.isin(('AP', 'ACP'))) & (data.rifaa)) | data.outdoor.isin(('ACEL', 'AEEL'))]
     return sel
 
 
-def autonomie_relative_piscine(data):
-    data = valid(data)
+def autonomie_relative_piscine(data, date='today'):
+    data = certif_valid_at(data, target_date=date)
     sel = data.loc[data.indoor.isin(('AP', 'ACP')) | data.outdoor.isin(('ACEL', 'AEEL'))]
     return sel
 
 
-def fosse(data):
-    data = valid(data)
+def fosse(data, date='today'):
+    data = certif_valid_at(data, target_date=date)
     sel = data.loc[(data.indoor.isin(('AP', 'ACP'))) | data.outdoor.isin(('AEL', 'ACEL', 'AEEL'))]
     return sel
 
 
 def rifaa(data):
+    """
+
+    :param data:
+    :return: The data for freediver who have a RIFAA
+    """
     sel = data.loc[data.rifaa]
     return sel
 
 
 def no_rifaa(data):
+    """
+
+    :param data:
+    :return: The data for freediver who have NOT RIFAA
+    """
     sel = data.loc[-data.rifaa]
     return sel
 
 
 def all(data):
+    """
+    return the
+    :param data:
+    :return:
+    """
     return to_emails(data)
 
 
@@ -52,21 +72,18 @@ def _certif_valid(data, delta):
     return data
 
 
-def _certif_valid_at(data, target_date='today'):
+def certif_valid_at(data, target_date='today'):
     """
-
-    :param data:
-    :param int delta:
-    :return:
+    :param data: the data related to the caci
+    :type data: pandas.DataFrame
+    :param str target_date: the date at which the caci should be valid
+    :return: the data for which the caci is valid
+    :rtype: pandas.DataFrame
     """
     data = data.loc[(data.CACI) | (data.QS)]
     target_date = pd.to_datetime(target_date).normalize()
     data = data.loc[target_date - data.date < 365]
     return data
-
-
-def valid(data, date='today'):
-    return _certif_valid_at(data)
 
 
 def warning(data):
