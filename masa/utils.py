fe315f0fb076b9ab0b8728ee03762f1123951216
freeh4cker <freeh4cker@gmail.com>
2022-01-01 18:35:29 +0100

    :sparkles: add utilities to check date provide on cmd line

---
+++ masa/utils.py
@@ -0,0 +1,32 @@
+##########################################################################
+# allow to filter MASA's freedivers given some criteria                  #
+# Copyright (C) 2022  Bertrand Néron                                     #
+#                                                                        #
+# This file is part of filter_apnee package.                             #
+#                                                                        #
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <https://www.gnu.org/licenses/>. #
+##########################################################################
+
+import argparse
+
+import pandas as pd
+
+
+def parse_date(value):
+    try:
+        pd.to_datetime(value).normalize()
+        return value
+    except Exception as err:
+        raise argparse.ArgumentTypeError(f"cannot parse '{value}' as date: {err}")
+
